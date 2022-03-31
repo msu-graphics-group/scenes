ecedc5f1735c90441de8b28f33dc678f3a0f9d9f
vfrolov <vfrolov@graphics.cs.msu.ru>
2022-03-31 13:59:23 +0300

    edit

--- sample_loader_app/run_bunny.sh
+++ sample_loader_app/run_bunny.sh
@@ -1,1 +1,1 @@
-/home/frol/hydra/hydra -inputlib "/home/frol/PROG/msu-graphics-group/scenes/01_simple_scenes" -statefile "bunny_cornell.xml" -out z_out_ori.png -nowindow 1
+/home/frol/hydra/hydra -inputlib "/home/frol/PROG/msu-graphics-group/scenes/01_simple_scenes" -statefile "bunny_cornell.xml" -out z_out.png -nowindow 1
