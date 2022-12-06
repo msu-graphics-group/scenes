96d7d8720019bebdefb9c20b072e7529433dfdcb
vfrolov <vfrolov@graphics.cs.msu.ru>
2022-12-06 18:12:51 +0300

    edit

--- 01_simple_scenes/imgdiff.py
+++ 01_simple_scenes/imgdiff.py
@@ -1,12 +1,15 @@
 import cv2
 import sys
+import os
+
+os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
 
 # load images
 image1 = cv2.imread(sys.argv[1])
 image2 = cv2.imread(sys.argv[2])
 
 # compute difference
-difference = cv2.subtract(image1, image2)*10
+difference = cv2.subtract(image1, image2)*10*255
 
 cv2.imshow('image', difference)
 cv2.waitKey(0)
