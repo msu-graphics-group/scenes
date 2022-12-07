d5c60b0735e2338503fad29d7886cc8136f2e437
vfrolov <vfrolov@graphics.cs.msu.ru>
2022-12-07 13:44:54 +0300

    compare HDR images

--- 01_simple_scenes/imgdiff.py
+++ 01_simple_scenes/imgdiff.py
@@ -1,17 +1,17 @@
-import cv2
 import sys
 import os
-
 os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
+import cv2
 
-# load images
-image1 = cv2.imread(sys.argv[1])
-image2 = cv2.imread(sys.argv[2])
+# then just type in following
+
+image1 = cv2.imread(sys.argv[1], cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+image2 = cv2.imread(sys.argv[2], cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
 
 # compute difference
-difference = cv2.subtract(image1, image2)*10*255
+difference = cv2.subtract(image1, image2)*10
 
-cv2.imshow('image', difference)
+cv2.imshow('diff*10', difference)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
