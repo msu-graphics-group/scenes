baf0b982acd767426124fe1af46587475ed5176a
vfrolov <vfrolov@graphics.cs.msu.ru>
2022-12-08 15:01:34 +0300

    ggx, both mitsuba and pbrt

--- 01_simple_scenes/mirender.py
+++ 01_simple_scenes/mirender.py
@@ -9,6 +9,6 @@ scene = mi.load_file(sys.argv[1])
 image = mi.render(scene, spp=1024)
 
 time.sleep(0.5)
-mi.util.write_bitmap("y05_cornell_mitsuba_lambert.png", image, write_async = False)
-mi.util.write_bitmap("y05_cornell_mitsuba_lambert.exr", image, write_async = False)
+mi.util.write_bitmap("y08_sphere_mitsuba3_ggx_metal.png", image, write_async = False)
+mi.util.write_bitmap("y08_sphere_mitsuba3_ggx_metal.exr", image, write_async = False)
 time.sleep(0.5)
