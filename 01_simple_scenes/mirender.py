7740aac4dfe64481de12d5d6a6ec0d01b7fc5151
Vladimir Frolov <vova@frolov.pp.ru>
2022-12-10 13:45:43 +0300

    gltf2Lite model

--- 01_simple_scenes/mirender.py
+++ 01_simple_scenes/mirender.py
@@ -4,11 +4,12 @@ import time
 
 #print(mi.variants()) # ['scalar_rgb', 'scalar_spectral', 'cuda_ad_rgb', 'llvm_ad_rgb']
 mi.set_variant("scalar_rgb")
-
 scene = mi.load_file(sys.argv[1])
 image = mi.render(scene, spp=1024)
 
+rawName = sys.argv[1].split(".")[0]
+
 time.sleep(0.5)
-mi.util.write_bitmap("y08_sphere_mitsuba3_ggx_metal.png", image, write_async = False)
-mi.util.write_bitmap("y08_sphere_mitsuba3_ggx_metal.exr", image, write_async = False)
+mi.util.write_bitmap("y" + rawName + ".png", image, write_async = False)
+mi.util.write_bitmap("y" + rawName + ".exr", image, write_async = False)
 time.sleep(0.5)
---
+++ 01_simple_scenes/run_hydra2.sh
@@ -0,0 +1,1 @@
+$HOME/hydra/hydra -inputlib "." -statefile "10_sphere_hydra_gltf2lite.xml" -out "y10_sphere_hydra2_gltf2lite.exr" -nowindow 1 
